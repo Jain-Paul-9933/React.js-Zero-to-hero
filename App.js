0d41189cbd412f39bd01556df979464941192fde
Jain-Paul-9933 <maliyakkalpauljain@gmail.com>
2023-08-15 12:06:10 +0530

    have done implementaion of react library through cdn links has used functons like reactDOM,createroot,createlement

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading1 = React.createElement("h1", { id: "heading1" }, "Hello React");
+const heading2 = React.createElement("h2", { id: "heading2" }, "Hi React");
+const container = React.createElement("div", { id: "container" }, [heading1, heading2])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
